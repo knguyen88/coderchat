771ae4dece43e82c9a845719d37d52fd42043c2c
Kha Nguyen <kha@tinypulse.com>
2016-03-17 22:38:22 +0700

    login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,29 @@
 class UsersController < ApplicationController
-  def login
 
+  def show_login_form
+    render 'users/login'
   end
 
-  def register
+  def do_login
+    @user = User.find_by_email(params[:email]).try(:authenticate, params[:password])
+    if !!@user
+      store_id_and_redirect_to_home(@user)
+    else
+      flash[:error] = 'Invalid email or password'
+      redirect_to login_path
+    end
+  end
+
+  def show_register_form
     @user = User.new
+    render 'users/register'
   end
 
-  def create
+  def do_create_user
     @user = User.new(user_params)
     if @user.valid?
       @user.save
-      session[:user_id] = @user.id
-      redirect_to root_path
+      store_id_and_redirect_to_home(@user)
     end
     render 'users/register'
   end
@@ -20,5 +31,10 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def store_id_and_redirect_to_home(user)
+    session[:user_id] = @user.id
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get '/login', to: 'users#login'
-  get '/register', to: 'users#register'
-  post '/register', to: 'users#create'
+  get '/login', to: 'users#show_login_form'
+  post '/login', to: 'users#do_login'
+  get '/register', to: 'users#show_register_form'
+  post '/register', to: 'users#do_create_user'
 end
